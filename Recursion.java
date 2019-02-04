f96192b00d528a7e6002373817ef962d57f84a42
TarzanRoboticsMan <dsontag00@stuy.edu>
2019-02-04 08:02:31 -0500

    called helper in sqrt, began to write helper

--- Recursion.java
+++ Recursion.java
@@ -1,27 +1,30 @@
 public class recursion{
-    /*You may write additional private methods */
+  /*You may write additional private methods */
 
-    /*Recursively find the sqrt using Newton's approximation
-     *tolerance is the allowed percent error the squared answer is away from n.
-     *precondition: n is non-negative
+  /*Recursively find the sqrt using Newton's approximation
+  *tolerance is the allowed percent error the squared answer is away from n.
+  *precondition: n is non-negative
 
-    */
-    public static double sqrt(double n, double tolerance){
+  */
+  public static double sqrt(double n, double tolerance){
+    return sqrtHelper(n, n/2, tolerance);
+  }
+  private double sqrtHelper(double n, double guess, double tolerance){
+    if(guess**2-n/n <= tolerance) return guess;
+  }
 
-    }
+  /*Recursively find the n'th fibbonaci number in linear time
+  *fib(0) = 0
+  *fib(1) = 1
+  *fib(5) = 5
+  *precondition: n is non-negative
+  */
+  public static int fib(int n){
 
-    /*Recursively find the n'th fibbonaci number in linear time
-     *fib(0) = 0
-     *fib(1) = 1
-     *fib(5) = 5
-     *precondition: n is non-negative
-     */
-    public static int fib(int n){
+  }
 
-    }
-
-    /*As Per classwork*/
-    public static ArrayList<Integer> makeAllSums(int n){
-    }
+  /*As Per classwork*/
+  public static ArrayList<Integer> makeAllSums(int n){
+  }
 
 }
